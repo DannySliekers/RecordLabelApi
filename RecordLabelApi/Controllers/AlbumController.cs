b444a79b52591abd4f944b860bd240d80c2fbca7
DannySliekers <59461479+DannySliekers@users.noreply.github.com>
2023-11-02 14:24:31 +0100

    add single and album controllers

--- RecordLabelApi/Controllers/AlbumController.cs
+++ RecordLabelApi/Controllers/AlbumController.cs
@@ -1,15 +1,83 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RecordLabelApi.Models;
+using RecordLabelApi.Repositories;
 
 namespace RecordLabelApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class AlbumController
+    public class AlbumController : ControllerBase
     {
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly ILogger<AlbumController> _logger;
+        private readonly IAlbumRepository _repository;
+
+        public AlbumController(ILogger<AlbumController> logger, IAlbumRepository repository)
         {
-            return Enumerable.Empty<string>();
+            _logger = logger;
+            _repository = repository;
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(200, Type = typeof(Album))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> DeleteAlbum(int id)
+        {
+            try
+            {
+                await _repository.DeleteAlbum(id);
+                return Ok();
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, "Error while deleting album");
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(200, Type = typeof(Album))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> AddAlbum(Album album)
+        {
+            try
+            {
+                await _repository.AddAlbum(album);
+                return Ok(album);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, "Error while inserting album");
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        [ProducesResponseType(200, Type = typeof(Album))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> UpdateAlbum(Album album)
+        {
+            try
+            {
+                await _repository.UpdateAlbum(album);
+                return Ok(album);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, "Error while updating album");
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        public List<Album> GetAlbums()
+        {
+            return _repository.GetAll().ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Album GetById(int id)
+        {
+            return _repository.Get(id);
         }
     }
 }
--- RecordLabelApi/Controllers/SingleController.cs
+++ RecordLabelApi/Controllers/SingleController.cs
@@ -1,15 +1,83 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RecordLabelApi.Repositories;
+using Single = RecordLabelApi.Models.Single;
 
 namespace RecordLabelApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class SingleController
+    public class SingleController : ControllerBase
     {
-        [HttpGet(Name = "GetSingles")]
-        public IEnumerable<string> Get()
+        private readonly ILogger<SingleController> _logger;
+        private readonly ISingleRepository _repository;
+
+        public SingleController(ILogger<SingleController> logger, ISingleRepository repository)
         {
-            return Enumerable.Empty<string>();
+            _logger = logger;
+            _repository = repository;
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(200, Type = typeof(Single))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> DeleteSingle(int id)
+        {
+            try
+            {
+                await _repository.DeleteSingle(id);
+                return Ok();
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, "Error while deleting single");
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(200, Type = typeof(Single))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> AddSingle(Single single)
+        {
+            try
+            {
+                await _repository.AddSingle(single);
+                return Ok(single);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, "Error while inserting single");
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        [ProducesResponseType(200, Type = typeof(Single))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> UpdateSingle(Single single)
+        {
+            try
+            {
+                await _repository.UpdateSingle(single);
+                return Ok(single);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, "Error while updating single");
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        public List<Single> GetSingles()
+        {
+            return _repository.GetAll().ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Single GetById(int id)
+        {
+            return _repository.Get(id);
         }
     } 
 }
