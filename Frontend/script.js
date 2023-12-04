55b7a9cd4b325008651301b35e42ebbf4443e603
DannySliekers <59461479+DannySliekers@users.noreply.github.com>
2023-12-04 21:57:30 +0100

    add artist page

--- Frontend/script.js
+++ Frontend/script.js
@@ -19,13 +19,13 @@ document.addEventListener("DOMContentLoaded", function() {
 function populateArtists(data) {
     var result = "";
     data.forEach(element => {
-        result += `<tr class='table-row' data-toggle='collapse' data-target='.row1-details' aria-expanded='false'>
+        result += `<tr class='table-row'>
         <td>
-          <img id='arrowOpen' height='30px' src='img/arrowclosed.png' alt='Closed arrow'>
-          <img id='arrowClosed' class='hide' width='30px' height='30px' src='img/arrowopen.png' alt='Open arrow'>
+          <img id='arrowOpen' data-toggle='collapse' data-target='.row1-details' aria-expanded='false' height='30px' src='img/arrowclosed.png' alt='Closed arrow'>
+          <img id='arrowClosed' class='hide' data-toggle='collapse' data-target='.row1-details' width='30px' height='30px' src='img/arrowopen.png' alt='Open arrow'>
         </td>
         <td>` + element.name + `</td>
-        <td><button>Edit</button></td>
+        <td><a href='artist.html?id=` + element.id + `'><button>Edit</button></a></td>
         <td><button>Delete</button></td>
       </tr>
       <tr id='artistDetails' class='details-row row1-details artistInspection collapse'>
