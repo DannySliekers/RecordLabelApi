7fdc1d24d9ddd87d5577690fca61248ce0fe24cb
DannySliekers <59461479+DannySliekers@users.noreply.github.com>
2025-01-05 15:05:06 +0100

    add authorize attribute to all controllers

--- RecordLabelApi/Controllers/PlatformController.cs
+++ RecordLabelApi/Controllers/PlatformController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RecordLabelApi.Models;
 using RecordLabelApi.Repositories;
 
 namespace RecordLabelApi.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class PlatformController : ControllerBase
--- RecordLabelApi/Controllers/SingleController.cs
+++ RecordLabelApi/Controllers/SingleController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RecordLabelApi.Repositories;
 using Single = RecordLabelApi.Models.Single;
 
 namespace RecordLabelApi.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class SingleController : ControllerBase
